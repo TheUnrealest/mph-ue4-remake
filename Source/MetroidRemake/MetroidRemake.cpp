2e20ffb01aa853bca59121f9aeae9eb5f4a657a4
Ninni <ninnius.24@outlook.it>
2024-03-05 07:43:28 +0100

    e riproviamo con un plugin
    todo: prima di personalizzare il codice, continua a scrivere riguardo la struttura su notion.
    librerie incluse ora

---
+++ Source/MetroidRemake/MetroidRemake.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MetroidRemake.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MetroidRemake, "MetroidRemake" );
