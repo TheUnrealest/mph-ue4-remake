e0c0916bea495c7d6d95a2a2722c67bcaa48e6c9
Ninni <ninnius.24@outlook.it>
2024-03-08 08:15:27 +0100

    altra prova

--- Plugins/JoyShockTest/Source/JoyShockTest/Private/JoyShockFunctions.cpp
+++ Plugins/JoyShockTest/Source/JoyShockTest/Private/JoyShockFunctions.cpp
@@ -2,36 +2,27 @@
 
 
 #include "JoyShockFunctions.h"
-#include "JoyShockLibrary.h"
 
-
-bool UJoyShockFunctions::GetImuState(FVector& AccelVector, FVector& GyroVector, const int deviceID)
+int UJoyShockFunctions::ConnectAndAssignUpdate(int & NumberOfDevices)
 {
-	IMU_STATE Test = JslGetIMUState(deviceID);
-	AccelVector.Set(Test.accelX, Test.accelY, Test.accelZ);
-	GyroVector.Set(Test.gyroX, Test.gyroY, Test.gyroZ);
-	return true;
+	// Alla connessione, dobbiamo connettere i dispositivi e trovare i loro handle.
+	NumberOfDevices = JslConnectDevices();
+	JslGetConnectedDeviceHandles(DevicesArray, NumberOfDevices);
+	// DEBUG: scriviamo su console il numero dei dispositivi...
+	UE_LOG(LogTemp, Display, TEXT("Numero dispositivi: %d"), NumberOfDevices);
+	// DEBUG: ... e i loro rispettivi handle.
+	UE_LOG(LogTemp, Display, TEXT("Handle dei dispositivi: %d", DevicesArray));
+	// Restituisco i numeri dei dispositivi.
+	return NumberOfDevices;
 }
 
-bool UJoyShockFunctions::IsStillConnected(int deviceID)
+bool UJoyShockFunctions::GetIMUValues(int deviceID, FVector & AccelOutput, FVector & GyroOutput)
 {
-	return JslStillConnected(deviceID);
+	
 }
 
-void UJoyShockFunctions::Update(int number, JOY_SHOCK_STATE S1, JOY_SHOCK_STATE S2, IMU_STATE IMU1, IMU_STATE IMU2, float DeltaTime)
-{
-}
-
-
-void UJoyShockFunctions::ConnectDevices()
-{
-	int x = JslConnectDevices();
-	UE_LOG(LogTemp, Display, TEXT("Numero controller: %d"), x);
-	JslSetCallback(&Update);
-}
-
-void UJoyShockFunctions::DisconnectDevices()
+void UJoyShockFunctions::Disconnect()
 {
 	JslDisconnectAndDisposeAll();
+	
 }
-
--- Plugins/JoyShockTest/Source/JoyShockTest/Public/JoyShockFunctions.h
+++ Plugins/JoyShockTest/Source/JoyShockTest/Public/JoyShockFunctions.h
@@ -6,29 +6,37 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "JoyShockLibrary.h"
 #include "JoyShockFunctions.generated.h"
+/*
+	Questa classe si interfaccia con JoyShockLibrary. Per prima cosa, connetti i controller con ConnectDevices(), anche se viene fatto durante il
+	caricamento dei moduli.
 
-/**
- * 
- */
+
+*/
+// Perché questi valori li devo definire fuori dalla classe? CHE SENSO HA?! SE IO CHIAMO JOYSHOCKFUNCTIONS, VOGLIO LE VARIABILI DI JOYSHOCKFUNCTIONS!
+// Ah, il C++. Classico.
+// (Penso che sia così perché questa classe viene costruita una volta, dato che è statica...)
+// https://stackoverflow.com/questions/12573816/what-is-an-undefined-reference-unresolved-external-symbol-error-and-how-do-i-fix/12574407#12574407
+
+// Queste variabili sono usate solo internamente, per trasferire valori da una funzione all'altra.
+
+static int NumberOfDevices; // Numero di dispositivi restituito alla connessione JslConnectDevices().
+static int DevicesArray[]; // Array di interi che contiene gli handle ai dispositivi. La sua lunghezza è definita a runtime.
+
 UCLASS()
 class JOYSHOCKTEST_API UJoyShockFunctions : public UBlueprintFunctionLibrary
 {
+	
+
 	GENERATED_BODY()
-	UFUNCTION(BlueprintCallable)
-		static bool GetImuState(
-			FVector& AccelVectorOutput,
-			FVector& GyroVectorOutput,
-			const int deviceID = 0
-		);
 
-	UFUNCTION(BlueprintPure)
-		static bool IsStillConnected(
-			int deviceID = 0
+	UFUNCTION(BlueprintCallable)
+		static int ConnectAndAssignUpdate(int &NumberOfDevices);
+	UFUNCTION(BlueprintCallable)
+		static bool GetIMUValues(
+			int deviceID,
+			FVector &AccelOutput,
+			FVector &GyroOutput
 		);
 	UFUNCTION(BlueprintCallable)
-		static void ConnectDevices();
-	UFUNCTION(BlueprintCallable)
-		static void DisconnectDevices();
-
-	static void Update(int number, JOY_SHOCK_STATE S1, JOY_SHOCK_STATE S2, IMU_STATE IMU1, IMU_STATE IMU2, float DeltaTime);
+		static void Disconnect();
 };
